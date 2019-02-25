2f831ad5a12e33e543cd15dd0f71146b6a2609be
Arifsky121 <37656226+Arifsky121@users.noreply.github.com>
2019-02-25 17:28:47 +0600

    Add files via upload

---
+++ server.R
@@ -0,0 +1,22 @@
+
+
+
+# Script to demonstrate distributions
+library(eeptools)
+library(shiny)
+library(ggplot2)
+
+
+rnormcor <- function(x,rho) rnorm(1,rho*x,sqrt(1-rho^2))
+
+shinyServer(function(input,output){
+  output$distPlot<-reactivePlot(function(){
+    a<-rnorm(input$obs)
+    b<-sapply(a,rnormcor,rho=input$rho)
+    p<-qplot(a,b,alpha=0.85)+geom_smooth(method="lm",se=FALSE,size=1.1)+theme_dpi()
+    p<-p+labs(x="",y="",title="Demonstrating Correlations")
+    p<-p+geom_text(aes(x=-2.5,y=3,label=paste("Corr. =",input$rho,sep=" ")),size=8)
+    print(p)
+    
+  })
+})
---
+++ ui.R
@@ -0,0 +1,29 @@
+
+#
+
+# Script to demonstrate distributions
+library(eeptools)
+library(shiny)
+library(ggplot2)
+shinyUI(pageWithSidebar(
+  
+  # Title
+  headerPanel("Simulating Data with Correlation"),
+  
+  sidebarPanel(
+    sliderInput("obs","Number of observations:",
+                min=200,max=5000,value=500,step=250),
+    sliderInput("rho","Correlation Coefficient",
+                min=-1,max=1,value=0,step=0.1)    
+  ),
+  
+  # GGPLOT
+  
+  mainPanel(
+    plotOutput("distPlot")
+  )
+  
+  
+  
+))
+
